d06bf1e53b71b9660853be7ec60927d3be5b006a
Rahman Yusuf <danipart4@gmail.com>
2020-12-25 02:09:04 +0700

    fixed test.py

--- test.py
+++ test.py
@@ -86,38 +86,38 @@ try:
 except AttributeError:
     class TestAsyncYoutubeSearch(unittest.TestCase):
         def test_with_given_time(self):
-            async def worker():
+            async def worker()():
                 y = AsyncYoutubeSearch('gordon ramsay', json_results=False, max_results=MAXIMUM_RESULTS, timeout=BASE_TIMEOUT)
                 data = await y.search()
                 await y.session.close()
                 return data
             loop = asyncio.new_event_loop()
-            data = loop.run_until_complete(worker)
+            data = loop.run_until_complete(worker())
             self.assertEqual(len(data), 10)
             self.assertIsInstance(data, list)
         
         def test_normal(self):
-            async def worker():
+            async def worker()():
                 y = AsyncYoutubeSearch('gordon ramsay', json_results=False, max_results=MAXIMUM_RESULTS)
                 data = await y.search()
                 await y.session.close()
                 return data
             loop = asyncio.new_event_loop()
-            data = loop.run_until_complete(worker)
+            data = loop.run_until_complete(worker())
             self.assertIsInstance(data, list)
 
         def test_with_included_related_videos(self):
-            async def worker():
+            async def worker()():
                 y = AsyncYoutubeSearch('gordon ramsay', json_results=False, max_results=MAXIMUM_RESULTS, include_related_videos=True)
                 data = await y.search()
                 await y.session.close()
                 return data
             loop = asyncio.new_event_loop()
-            data = loop.run_until_complete(worker)
+            data = loop.run_until_complete(worker())
             self.assertIsInstance(data, list)
 
         def test_all_user_agents(self):
-            async def worker():
+            async def worker()():
                 for ua in USER_AGENT_HEADERS.keys():
                     session = AsyncYoutubeSession(ua)
                     await session.new_session()
@@ -125,11 +125,11 @@ except AttributeError:
                     await youtube.search()
                     await youtube.session.close()
             loop = asyncio.new_event_loop()
-            loop.run_until_complete(worker)
+            loop.run_until_complete(worker())
             
 
         def test_all_user_agents_with_related_videos(self):
-            async def worker():
+            async def worker()():
                 for ua in USER_AGENT_HEADERS.keys():
                     session = AsyncYoutubeSession(ua)
                     await session.new_session()
@@ -137,7 +137,43 @@ except AttributeError:
                     await youtube.search()
                     await youtube.session.close()
             loop = asyncio.new_event_loop()
-            loop.run_until_complete(worker)
+            loop.run_until_complete(worker())
+except ValueError:
+    class TestAsyncYoutubeSearch(unittest.IsolatedAsyncioTestCase):
+        async def test_with_given_time(self):
+            y = AsyncYoutubeSearch('gordon ramsay', json_results=False, max_results=MAXIMUM_RESULTS, timeout=BASE_TIMEOUT)
+            data = await y.search()
+            await y.session.close()
+            self.assertEqual(len(data), 10)
+            self.assertIsInstance(data, list)
+        
+        async def test_normal(self):
+            y = AsyncYoutubeSearch('gordon ramsay', json_results=False, max_results=MAXIMUM_RESULTS)
+            data = await y.search()
+            await y.session.close()
+            self.assertIsInstance(data, list)
+
+        async def test_with_included_related_videos(self):
+            y = AsyncYoutubeSearch('gordon ramsay', json_results=False, max_results=MAXIMUM_RESULTS, include_related_videos=True)
+            data = await y.search()
+            await y.session.close()
+            self.assertIsInstance(data, list)
+
+        async def test_all_user_agents(self):
+            for ua in USER_AGENT_HEADERS.keys():
+                session = AsyncYoutubeSession(ua)
+                await session.new_session()
+                youtube = AsyncYoutubeSearch('gordon ramsay', max_results=MAXIMUM_RESULTS, json_results=False, async_youtube_session=session)
+                await youtube.search()
+                await youtube.session.close()
+
+        async def test_all_user_agents_with_related_videos(self):
+            for ua in USER_AGENT_HEADERS.keys():
+                session = AsyncYoutubeSession(ua)
+                await session.new_session()
+                youtube = AsyncYoutubeSearch('gordon ramsay', max_results=MAXIMUM_RESULTS, json_results=False, async_youtube_session=session, include_related_videos=True)
+                await youtube.search()
+                await youtube.session.close()
 
 if __name__ == "__main__":
     unittest.main()

83afcdc49ce447d14fd1a8783ad17c6aea36c5b6
Rahman Yusuf <danipart4@gmail.com>
2021-02-13 16:02:21 +0700

    fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 import sys
 
-__VERSION__ = 'v0.1.0'
+__VERSION__ = 'v0.1.01'
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()

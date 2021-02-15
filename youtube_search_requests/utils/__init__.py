a381d79492ca5169a0afdb72036c711a568261a1
Rahman Yusuf <danipart4@gmail.com>
2021-02-15 23:59:57 +0700

    added YoutubePreferenceCookies
    for parsing preference youtube,
    such as "language", "restricted-mode", etc.

--- youtube_search_requests/utils/__init__.py
+++ youtube_search_requests/utils/__init__.py
@@ -9,6 +9,45 @@ from youtube_search_requests.constants import (
 )
 from youtube_search_requests.utils.errors import InvalidArgument
 
+class YoutubePreferenceCookies:
+    def __init__(self):
+        self.cookie = {'PREF': {}}
+
+    def _parse_preference(self, p: str):
+        if '&' in p:
+            pref = {}
+            for i in p.split('&'):
+                a = i.split('=')
+                pref[a[0]] = a[1]
+            return pref
+        else:
+            indexes = p.split('=')
+            return indexes[0], indexes[1]
+
+    def add_preference(self, name: str, value: str):
+        self.cookie['PREF'][name] = value
+
+    def add_preferences(self, preference: str):
+        PREF = self._parse_preference(preference)
+        if isinstance(PREF, dict):
+            for key in PREF.keys():
+                self.cookie['PREF'][key] = PREF[key]
+        else:
+            self.cookie['PREF'][PREF[0]] = PREF[1]
+
+    def get_cookie(self):
+        PREF = self.cookie['PREF']
+        a = ''
+        if len(PREF.keys()) == 1:
+            for key in PREF.keys():
+                a += key + '=' + PREF[key]
+        elif len(PREF.keys()) == 0:
+            return {}
+        else:
+            for key in PREF.keys():
+                a += key + '=' + PREF[key] + '&'
+        return {'PREF': a}
+
 def parse_json_session_data(r):
     d = r.text[r.text.find('ytcfg.set({') + 10:]
     return json.loads(d[0:d.find(');')])
